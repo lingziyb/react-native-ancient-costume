f9f17d62c4286adc878a82670a7d866fd7acc722
Zhaolingli <1101077695@qq.com>
2018-05-17 21:57:10 +0800

    做底部导航菜单

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import {
-	Text, Image, View, TextInput
+	Text, View, StyleSheet
 } from 'react-native';
 
-export default class App extends Component {
-	constructor( props ) {
-		super( props );
-		this.state = { text: '111 2222 333' };
-	}
+import HomePage from './js/pages/home';
 
+export default class App extends Component {
 	render() {
 		return (
-			<View>
-				<TextInput style={{ height: 40 }} placeholder="input your phone number..."/>
-				<Text style={{ padding: 10, backgroundColor: 'red' }}>
-					{this.state.text.split( ' ' ).map( ( word ) => word ).join( '' )}
-				</Text>
-			</View>
+			<HomePage/>
 		);
 	}
 }
-
---
+++ js/pages/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image
+} from 'react-native';
+
+export default class HomePage extends Component {
+	constructor(props){
+		super(props);
+	}
+	render() {
+		return (
+			<Text>welcome to linli home ~~</Text>
+		);
+	}
+}
